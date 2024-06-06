RitzDaCat/scx

commit 6f4cd853f97ad5c8d29e4047e9a2fb389a11c54e

scx_rustland: introduce virtual time slice
Overview
========

Currently, a task's time slice is determined based on the total number
of tasks waiting to be scheduled: the more overloaded the system, the
shorter the time slice.

This approach can help to reduce the average wait time of all tasks,
allowing them to progress more slowly, but uniformly, thus providing a
smoother overall system performance.

However, under heavy system load, this approach can lead to very short
time slices distributed among all tasks, causing excessive context
switches that can badly affect soft real-time workloads.

Moreover, the scheduler tends to operate in a bursty manner (tasks are
queued and dispatched in bursts). This can also result in fluctuations
of longer and shorter time slices, depending on the number of tasks
still waiting in the scheduler's queue.

Such behavior can also negatively impact on soft real-time workloads,
such as real-time audio processing.

Virtual time slice
==================

To mitigate this problem, introduce the concept of virtual time slice:
the idea is to evaluate the optimal time slice of a task, considering
the vruntime as a deadline for the task to complete its work before
releasing the CPU.

This is accomplished by calculating the difference between the task's
vruntime and the global current vruntime and use this value as the task
time slice:

  task_slice = task_vruntime - min_vruntime

In this way, tasks that "promise" to release the CPU quickly (based on
their previous work pattern) get a much higher priority (due to
vruntime-based scheduling and the additional priority boost for being
classified as interactive), but they are also given a shorter time slice
to complete their work and fulfill their promise of rapidity.

At the same time tasks that are more CPU-intensive get de-prioritized,
but they will tend to have a longer time slice available, reducing in
this way the amount of context switches that can negatively affect their
performance.

scheds/rust/scx_rustland/src/main.rs
@@ -497,22 +497,8 @@ impl<'a> Scheduler<'a> {
 
     // Return the target time slice, proportionally adjusted based on the total amount of tasks
     // waiting to be scheduled (more tasks waiting => shorter time slice).
-    fn effective_slice_ns(&mut self, nr_scheduled: u64) -> u64 {
-        // Scale time slice as a function of nr_scheduled, but never scale below 250 us.
-        //
-        // The goal here is to adjust the time slice allocated to tasks based on the number of
-        // tasks currently awaiting scheduling. When the system is heavily loaded, shorter time
-        // slices are assigned to provide more opportunities for all tasks to receive CPU time.
-        let scaling = ((nr_scheduled + 1) / 2).max(1);
-        let slice_ns = (self.slice_ns / scaling).max(NSEC_PER_MSEC / 4);
-
-        slice_ns
-    }
-
     // Dispatch tasks from the task pool in order (sending them to the BPF dispatcher).
     fn dispatch_tasks(&mut self) {
-        let nr_scheduled = self.task_pool.tasks.len() as u64;
-
         // Dispatch only a batch of tasks equal to the amount of idle CPUs in the system.
         //
         // This allows to have more tasks sitting in the task pool, reducing the pressure on the
@@ -521,28 +507,58 @@ impl<'a> Scheduler<'a> {
         for _ in 0..self.nr_idle_cpus().max(1) {
             match self.task_pool.pop() {
                 Some(task) => {
+                    // Determine the task's virtual time slice.
+                    //
+                    // The goal is to evaluate the optimal time slice, considering the vruntime as
+                    // a deadline for the task to complete its work before releasing the CPU.
+                    //
+                    // This is accomplished by calculating the difference between the task's
+                    // vruntime and the global current vruntime and use this value as the task time
+                    // slice.
+                    //
+                    // In this way, tasks that "promise" to release the CPU quickly (based on
+                    // their previous work pattern) get a much higher priority (due to
+                    // vruntime-based scheduling and the additional priority boost for being
+                    // classified as interactive), but they are also given a shorter time slice
+                    // to complete their work and fulfill their promise of rapidity.
+                    //
+                    // At the same time tasks that are more CPU-intensive get de-prioritized, but
+                    // they will also tend to have a longer time slice available, reducing in this
+                    // way the amount of context switches that can negatively affect their
+                    // performance.
+                    //
+                    // In conclusion, latency-sensitive tasks get a high priority and a short time
+                    // slice (and they can preempt other tasks), CPU-intensive tasks get low
+                    // priority and a long time slice.
+                    //
+                    // Moreover, ensure that the time slice is never less than 0.25 ms to prevent
+                    // excessive penalty from assigning time slices that are too short and reduce
+                    // context switch overhead.
+                    let slice_ns = (task.vruntime - self.min_vruntime).max(NSEC_PER_MSEC / 4);
+
                     // Update global minimum vruntime.
                     self.min_vruntime = task.vruntime;
 
+                    // Create a new task to dispatch.
                     let mut dispatched_task = DispatchedTask::new(&task.qtask);
 
-                    // Interactive tasks will be dispatched on the first CPU available and they are
-                    // allowed to preempt other tasks.
+                    dispatched_task.set_slice_ns(slice_ns);
+
                     if task.is_interactive {
+                        // Dispatch interactive tasks on the first CPU available.
                         dispatched_task.set_flag(RL_CPU_ANY);
+
+                        // Interactive tasks can preempt other tasks.
                         if !self.no_preemption {
                             dispatched_task.set_flag(RL_PREEMPT_CPU);
                         }
                     }
+
+                    // In full-user mode we skip the built-in idle selection logic, so simply
+                    // dispatch all the tasks on the first CPU available.
                     if self.full_user {
-                        // In full-user mode we skip the built-in idle selection logic, so simply
-                        // dispatch all the tasks on the first CPU available.
                         dispatched_task.set_flag(RL_CPU_ANY);
                     }
-
-                    // Assign a timeslice as a function of the amount of tasks that are waiting to
-                    // be scheduled.
-                    dispatched_task.set_slice_ns(self.effective_slice_ns(nr_scheduled));
 
                     // Send task to the BPF dispatcher.
                     match self.bpf.dispatch_task(&dispatched_task) {

commit 44441153a206fe311cfe90e3015d7319e16e516a

scx_rustland: never use a time slice that exceeds the default value
Make sure to never assign a time slice longer than the default time
slice, that can be used as an upper limit.

This seems to prevent potential stall conditions (reported by the
CachyOS community) when running CPU-intensive workloads, such as:

 [   68.062813] sched_ext: BPF scheduler "rustland" errored, disabling
 [   68.062831] sched_ext: runnable task stall (ollama_llama_se[3312] failed to run for 5.180s)
 [   68.062832]    scx_watchdog_workfn+0x154/0x1e0
 [   68.062837]    process_one_work+0x18e/0x350
 [   68.062839]    worker_thread+0x2fa/0x490
 [   68.062841]    kthread+0xd2/0x100
 [   68.062842]    ret_from_fork+0x34/0x50
 [   68.062844]    ret_from_fork_asm+0x1a/0x30

Fixes: 6f4cd853 ("scx_rustland: introduce virtual time slice")
Tested-by: SoulHarsh007 <harsh.peshwani@outlook.com>
Tested-by: Piotr Gorski <piotrgorski@cachyos.org>
Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

scheds/rust/scx_rustland/src/main.rs
@@ -534,7 +534,8 @@ impl<'a> Scheduler<'a> {
                     // Moreover, ensure that the time slice is never less than 0.25 ms to prevent
                     // excessive penalty from assigning time slices that are too short and reduce
                     // context switch overhead.
-                    let slice_ns = (task.vruntime - self.min_vruntime).max(NSEC_PER_MSEC / 4);
+                    let slice_ns =
+                        (task.vruntime - self.min_vruntime).clamp(NSEC_PER_MSEC / 4, self.slice_ns);
 
                     // Update global minimum vruntime.
                     self.min_vruntime = task.vruntime;