RitzDaCat/scx

commit 263e02f644b5bb8c8a01c81b0802772341b4e5fd

rusty: Use nr_cpu_ids instead of nr_cpus_possible
In scx_rusty, we're currently using topo.nr_cpus_possible() to determine
how many possible CPU IDs we could have on the system. scx_rusty already
accounts for offlined CPUs, so to properly support systems whose
disabled CPUs may be in the middle of the range of possible CPU IDs,
let's instead use topo.nr_cpu_ids().

Signed-off-by: David Vernet <void@manifault.com>

scheds/rust/scx_rusty/src/main.rs
@@ -243,10 +243,10 @@ impl<'a> Scheduler<'a> {
 
         let domains = Arc::new(DomainGroup::new(top.clone(), &opts.cpumasks)?);
 
-        if top.nr_cpus_possible() > MAX_CPUS {
+        if top.nr_cpu_ids() > MAX_CPUS {
             bail!(
-                "Num possible CPUs ({}) exceeds maximum of ({})",
-                top.nr_cpus_possible(),
+                "Num possible CPU IDs ({}) exceeds maximum of ({})",
+                top.nr_cpu_ids(),
                 MAX_CPUS
             );
         }
@@ -261,13 +261,13 @@ impl<'a> Scheduler<'a> {
 
         skel.rodata_mut().nr_nodes = domains.nr_nodes() as u32;
         skel.rodata_mut().nr_doms = domains.nr_doms() as u32;
-        skel.rodata_mut().nr_cpus_possible = top.nr_cpus_possible() as u32;
+        skel.rodata_mut().nr_cpu_ids = top.nr_cpu_ids() as u32;
 
         // Any CPU with dom > MAX_DOMS is considered offline by default. There
         // are a few places in the BPF code where we skip over offlined CPUs
         // (e.g. when initializing or refreshing tune params), and elsewhere the
         // scheduler will error if we try to schedule from them.
-        for cpu in 0..top.nr_cpus_possible() {
+        for cpu in 0..top.nr_cpu_ids() {
             skel.rodata_mut().cpu_dom_id_map[cpu] = u32::MAX;
         }
 
@@ -413,7 +413,7 @@ impl<'a> Scheduler<'a> {
         let stats_map = maps.stats();
         let mut stats: Vec<u64> = Vec::new();
         let zero_vec =
-            vec![vec![0u8; stats_map.value_size() as usize]; self.top.nr_cpus_possible()];
+            vec![vec![0u8; stats_map.value_size() as usize]; self.top.nr_cpu_ids()];
 
         for stat in 0..bpf_intf::stat_idx_RUSTY_NR_STATS {
             let cpu_stat_vec = stats_map

scheds/rust/scx_rusty/src/bpf/main.bpf.c
@@ -60,7 +60,7 @@ UEI_DEFINE(uei);
  */
 const volatile u32 nr_doms = 32;	/* !0 for veristat, set during init */
 const volatile u32 nr_nodes = 32;	/* !0 for veristat, set during init */
-const volatile u32 nr_cpus_possible = 64;	/* !0 for veristat, set during init */
+const volatile u32 nr_cpu_ids = 64;	/* !0 for veristat, set during init */
 const volatile u32 cpu_dom_id_map[MAX_CPUS];
 const volatile u32 dom_numa_id_map[MAX_DOMS];
 const volatile u64 dom_cpumasks[MAX_DOMS][MAX_CPUS / 64];
@@ -493,7 +493,7 @@ static void refresh_tune_params(void)
 	tune_params_gen = tune_input.gen;
 	slice_ns = tune_input.slice_ns;
 
-	bpf_for(cpu, 0, nr_cpus_possible) {
+	bpf_for(cpu, 0, nr_cpu_ids) {
 		u32 dom_id = cpu_to_dom_id(cpu);
 		struct dom_ctx *domc;
 
@@ -1784,7 +1784,7 @@ s32 BPF_STRUCT_OPS_SLEEPABLE(rusty_init)
 			return ret;
 	}
 
-	bpf_for(i, 0, nr_cpus_possible) {
+	bpf_for(i, 0, nr_cpu_ids) {
 		if (is_offline_cpu(i))
 			continue;
 

commit 1e56850c3984cd74d2f7c8a994aedb06520222ac

scx_rusty: fix stats map initialization
The stats map in scx_rusty is a BPF_MAP_TYPE_PERCPU_ARRAY, with its size
determined by num_possible_cpus(). Initializing it with nr_cpu_ids() can
result in errors such as:

 Error: Failed to zero stat

 Caused by:
     number of values 6 != number of cpus 8

Fix by using num_possible_cpus() to initialize it.

Fixes: 263e02f6 ("rusty: Use nr_cpu_ids instead of nr_cpus_possible")
Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

scheds/rust/scx_rusty/src/main.rs
@@ -485,7 +485,7 @@ impl<'a> Scheduler<'a> {
         let stats_map = maps.stats();
         let mut stats: Vec<u64> = Vec::new();
         let zero_vec =
-            vec![vec![0u8; stats_map.value_size() as usize]; self.top.nr_cpu_ids()];
+            vec![vec![0u8; stats_map.value_size() as usize]; self.top.nr_cpus_possible()];
 
         for stat in 0..bpf_intf::stat_idx_RUSTY_NR_STATS {
             let cpu_stat_vec = stats_map