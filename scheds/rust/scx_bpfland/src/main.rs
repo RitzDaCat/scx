RitzDaCat/scx

commit 1da249f063cec0a8940c2bc188e5dd333eb35bbd

scx_utils::topology: Always use NR_CPU_IDS and NR_CPUS_POSSIBLE
Always use the LazyLock versions and drop the counterparts from Topology.

scheds/rust/scx_bpfland/src/main.rs
@@ -44,9 +44,10 @@ use scx_utils::scx_ops_load;
 use scx_utils::scx_ops_open;
 use scx_utils::uei_exited;
 use scx_utils::uei_report;
-use scx_utils::UserExitInfo;
 use scx_utils::Cpumask;
 use scx_utils::Topology;
+use scx_utils::UserExitInfo;
+use scx_utils::NR_CPU_IDS;
 
 const SCHEDULER_NAME: &'static str = "scx_bpfland";
 
@@ -75,13 +76,15 @@ fn get_primary_cpus(powersave: bool) -> std::io::Result<Vec<usize>> {
         cpu_freqs.into_iter().map(|(cpu_id, _)| cpu_id).collect()
     } else if powersave {
         // If powersave is true, return the CPUs with the smallest frequency.
-        cpu_freqs.into_iter()
+        cpu_freqs
+            .into_iter()
             .filter(|&(_, freq)| freq == min_freq)
             .map(|(cpu_id, _)| cpu_id)
             .collect()
     } else {
         // If powersave is false, return the CPUs with the highest frequency.
-        cpu_freqs.into_iter()
+        cpu_freqs
+            .into_iter()
             .filter(|&(_, freq)| freq != min_freq)
             .map(|(cpu_id, _)| cpu_id)
             .collect()
@@ -110,7 +113,8 @@ fn cpus_to_cpumask(cpus: &Vec<usize>) -> String {
     }
 
     // Convert the byte vector to a hexadecimal string.
-    let hex_str: String = bitmask.iter()
+    let hex_str: String = bitmask
+        .iter()
         .rev()
         .map(|byte| format!("{:02x}", byte))
         .collect();
@@ -376,10 +380,14 @@ impl<'a> Scheduler<'a> {
         Ok(())
     }
 
-    fn init_primary_domain(skel: &mut BpfSkel<'_>, topo: &Topology, primary_domain: &Cpumask) -> Result<()> {
+    fn init_primary_domain(
+        skel: &mut BpfSkel<'_>,
+        topo: &Topology,
+        primary_domain: &Cpumask,
+    ) -> Result<()> {
         info!("primary CPU domain = 0x{:x}", primary_domain);
 
-        for cpu in 0..topo.nr_cpu_ids() {
+        for cpu in 0..*NR_CPU_IDS {
             if primary_domain.test_cpu(cpu) {
                 if let Err(err) = Self::enable_primary_cpu(skel, cpu) {
                     warn!("failed to add CPU {} to primary domain: error {}", cpu, err);
@@ -423,7 +431,7 @@ impl<'a> Scheduler<'a> {
         skel: &mut BpfSkel<'_>,
         topo: &Topology,
         cache_lvl: usize,
-        enable_sibling_cpu_fn: &dyn Fn(&mut BpfSkel<'_>, usize, usize, usize) -> Result<(), u32>
+        enable_sibling_cpu_fn: &dyn Fn(&mut BpfSkel<'_>, usize, usize, usize) -> Result<(), u32>,
     ) -> Result<(), std::io::Error> {
         // Determine the list of CPU IDs associated to each cache node.
         let mut cache_id_map: HashMap<usize, Vec<usize>> = HashMap::new();
@@ -450,7 +458,7 @@ impl<'a> Scheduler<'a> {
             for cpu in &cpus {
                 for sibling_cpu in &cpus {
                     match enable_sibling_cpu_fn(skel, cache_lvl, *cpu, *sibling_cpu) {
-                        Ok(()) => {},
+                        Ok(()) => {}
                         Err(_) => {
                             warn!(
                                 "L{} cache ID {}: failed to set CPU {} sibling {}",
@@ -465,13 +473,19 @@ impl<'a> Scheduler<'a> {
         Ok(())
     }
 
-    fn init_l2_cache_domains(skel: &mut BpfSkel<'_>, topo: &Topology) -> Result<(), std::io::Error> {
+    fn init_l2_cache_domains(
+        skel: &mut BpfSkel<'_>,
+        topo: &Topology,
+    ) -> Result<(), std::io::Error> {
         Self::init_cache_domains(skel, topo, 2, &|skel, lvl, cpu, sibling_cpu| {
             Self::enable_sibling_cpu(skel, lvl, cpu, sibling_cpu)
         })
     }
 
-    fn init_l3_cache_domains(skel: &mut BpfSkel<'_>, topo: &Topology) -> Result<(), std::io::Error> {
+    fn init_l3_cache_domains(
+        skel: &mut BpfSkel<'_>,
+        topo: &Topology,
+    ) -> Result<(), std::io::Error> {
         Self::init_cache_domains(skel, topo, 3, &|skel, lvl, cpu, sibling_cpu| {
             Self::enable_sibling_cpu(skel, lvl, cpu, sibling_cpu)
         })

scheds/rust/scx_rusty/src/main.rs
@@ -57,6 +57,8 @@ use scx_utils::Cpumask;
 use scx_utils::LogRecorderBuilder;
 use scx_utils::Topology;
 use scx_utils::UserExitInfo;
+use scx_utils::NR_CPUS_POSSIBLE;
+use scx_utils::NR_CPU_IDS;
 
 const MAX_DOMS: usize = bpf_intf::consts_MAX_DOMS as usize;
 const MAX_CPUS: usize = bpf_intf::consts_MAX_CPUS as usize;
@@ -295,8 +297,6 @@ struct Scheduler<'a> {
     balance_load: bool,
     balanced_kworkers: bool,
 
-    top: Arc<Topology>,
-
     dom_group: Arc<DomainGroup>,
 
     proc_reader: procfs::ProcReader,
@@ -324,14 +324,12 @@ impl<'a> Scheduler<'a> {
         let mut skel = scx_ops_open!(skel_builder, open_object, rusty).unwrap();
 
         // Initialize skel according to @opts.
-        let top = Arc::new(Topology::new()?);
+        let domains = Arc::new(DomainGroup::new(&Topology::new()?, &opts.cpumasks)?);
 
-        let domains = Arc::new(DomainGroup::new(top.clone(), &opts.cpumasks)?);
-
-        if top.nr_cpu_ids() > MAX_CPUS {
+        if *NR_CPU_IDS > MAX_CPUS {
             bail!(
                 "Num possible CPU IDs ({}) exceeds maximum of ({})",
-                top.nr_cpu_ids(),
+                *NR_CPU_IDS,
                 MAX_CPUS
             );
         }
@@ -346,13 +344,13 @@ impl<'a> Scheduler<'a> {
 
         skel.maps.rodata_data.nr_nodes = domains.nr_nodes() as u32;
         skel.maps.rodata_data.nr_doms = domains.nr_doms() as u32;
-        skel.maps.rodata_data.nr_cpu_ids = top.nr_cpu_ids() as u32;
+        skel.maps.rodata_data.nr_cpu_ids = *NR_CPU_IDS as u32;
 
         // Any CPU with dom > MAX_DOMS is considered offline by default. There
         // are a few places in the BPF code where we skip over offlined CPUs
         // (e.g. when initializing or refreshing tune params), and elsewhere the
         // scheduler will error if we try to schedule from them.
-        for cpu in 0..top.nr_cpu_ids() {
+        for cpu in 0..*NR_CPU_IDS {
             skel.maps.rodata_data.cpu_dom_id_map[cpu] = u32::MAX;
         }
 
@@ -423,7 +421,6 @@ impl<'a> Scheduler<'a> {
             balance_load: !opts.no_load_balance,
             balanced_kworkers: opts.balanced_kworkers,
 
-            top,
             dom_group: domains.clone(),
             proc_reader,
 
@@ -499,8 +496,7 @@ impl<'a> Scheduler<'a> {
     fn read_bpf_stats(&mut self) -> Result<Vec<u64>> {
         let stats_map = &mut self.skel.maps.stats;
         let mut stats: Vec<u64> = Vec::new();
-        let zero_vec =
-            vec![vec![0u8; stats_map.value_size() as usize]; self.top.nr_cpus_possible()];
+        let zero_vec = vec![vec![0u8; stats_map.value_size() as usize]; *NR_CPUS_POSSIBLE];
 
         for stat in 0..bpf_intf::stat_idx_RUSTY_NR_STATS {
             let cpu_stat_vec = stats_map

rust/scx_utils/src/topology.rs
@@ -78,10 +78,20 @@ use std::path::Path;
 use std::slice::Iter;
 use std::sync::LazyLock;
 
+/// The maximum possible number of CPU IDs in the system. As mentioned above,
+/// this is different than the number of possible CPUs on the system (though
+/// very seldom is). This number may differ from the number of possible CPUs on
+/// the system when e.g. there are fully disabled CPUs in the middle of the
+/// range of possible CPUs (i.e. CPUs that may not be onlined).
 pub static NR_CPU_IDS: LazyLock<usize> = LazyLock::new(|| {
     read_cpu_ids().unwrap().last().unwrap() + 1
 });
 
+/// The number of possible CPUs that may be active on the system. Note that this
+/// value is separate from the number of possible _CPU IDs_ in the system, as
+/// there may be gaps in what CPUs are allowed to be onlined. For example, some
+/// BIOS implementations may report spans of disabled CPUs that may not be
+/// onlined, whose IDs are lower than the IDs of other CPUs that may be onlined.
 pub static NR_CPUS_POSSIBLE: LazyLock<usize> =
     LazyLock::new(|| libbpf_rs::num_possible_cpus().unwrap());
 
@@ -231,9 +241,7 @@ pub struct Topology {
     cores: Vec<Core>,
     cpus: BTreeMap<usize, Cpu>,
     span: Cpumask,
-    nr_cpus_possible: usize,
     nr_cpus_online: usize,
-    nr_cpu_ids: usize,
 }
 
 impl Topology {
@@ -243,7 +251,7 @@ impl Topology {
         // If the kernel is compiled with CONFIG_NUMA, then build a topology
         // from the NUMA hierarchy in sysfs. Otherwise, just make a single
         // default node of ID 0 which contains all cores.
-        let (nodes, nr_cpu_ids) = if Path::new("/sys/devices/system/node").exists() {
+        let nodes = if Path::new("/sys/devices/system/node").exists() {
             create_numa_nodes(&span)?
         } else {
             create_default_node(&span)?
@@ -269,16 +277,13 @@ impl Topology {
             }
         }
 
-        let nr_cpus_possible = libbpf_rs::num_possible_cpus().unwrap();
         let nr_cpus_online = span.weight();
         Ok(Topology {
             nodes,
             cores,
             cpus,
             span,
-            nr_cpus_possible,
             nr_cpus_online,
-            nr_cpu_ids,
         })
     }
 
@@ -302,31 +307,11 @@ impl Topology {
         &self.span
     }
 
-    /// Get the number of possible CPUs that may be active on the system. Note
-    /// that this value is separate from the number of possible _CPU IDs_ in the
-    /// system, as there may be gaps in what CPUs are allowed to be onlined. For
-    /// example, some BIOS implementations may report spans of disabled CPUs
-    /// that may not be onlined, whose IDs are lower than the IDs of other CPUs
-    /// that may be onlined.
-    pub fn nr_cpus_possible(&self) -> usize {
-        self.nr_cpus_possible
-    }
-
     /// Get the number of CPUs that were online when the Topology was created.
     /// Because Topology objects are read-only, this value will not change if a
     /// CPU is onlined after a Topology object has been created.
     pub fn nr_cpus_online(&self) -> usize {
         self.nr_cpus_online
-    }
-
-    /// Get the maximum possible number of CPU IDs in the system. As mentioned
-    /// above, this is different than the number of possible CPUs on the system
-    /// (though very seldom is). This number may differ from the number of
-    /// possible CPUs on the system when e.g. there are fully disabled CPUs in
-    /// the middle of the range of possible CPUs (i.e. CPUs that may not be
-    /// onlined).
-    pub fn nr_cpu_ids(&self) -> usize {
-        self.nr_cpu_ids
     }
 }
 
@@ -503,7 +488,7 @@ fn read_cpu_ids() -> Result<Vec<usize>> {
     Ok(cpu_ids)
 }
 
-fn create_default_node(online_mask: &Cpumask) -> Result<(Vec<Node>, usize)> {
+fn create_default_node(online_mask: &Cpumask) -> Result<Vec<Node>> {
     let mut nodes: Vec<Node> = Vec::with_capacity(1);
     let mut node = Node {
         id: 0,
@@ -522,13 +507,12 @@ fn create_default_node(online_mask: &Cpumask) -> Result<(Vec<Node>, usize)> {
 
     nodes.push(node);
 
-    Ok((nodes, cpu_ids.last().unwrap() + 1))
+    Ok(nodes)
 }
 
-fn create_numa_nodes(online_mask: &Cpumask) -> Result<(Vec<Node>, usize)> {
+fn create_numa_nodes(online_mask: &Cpumask) -> Result<Vec<Node>> {
     let mut nodes: Vec<Node> = Vec::new();
 
-    let mut nr_cpu_ids = 0;
     let numa_paths = glob("/sys/devices/system/node/node*")?;
     for numa_path in numa_paths.filter_map(Result::ok) {
         let numa_str = numa_path.to_str().unwrap().trim();
@@ -556,14 +540,10 @@ fn create_numa_nodes(online_mask: &Cpumask) -> Result<(Vec<Node>, usize)> {
                 }
             };
 
-            if cpu_id + 1 > nr_cpu_ids {
-                nr_cpu_ids = cpu_id + 1;
-            }
-
             create_insert_cpu(cpu_id, &mut node, &online_mask)?;
         }
 
         nodes.push(node);
     }
-    Ok((nodes, nr_cpu_ids))
+    Ok(nodes)
 }

scheds/rust/scx_rusty/src/domain.rs
@@ -3,7 +3,6 @@
 // This software may be used and distributed according to the terms of the
 // GNU General Public License version 2.
 use std::collections::BTreeMap;
-use std::sync::Arc;
 
 use anyhow::Result;
 
@@ -49,7 +48,7 @@ pub struct DomainGroup {
 }
 
 impl DomainGroup {
-    pub fn new(top: Arc<Topology>, cpumasks: &[String]) -> Result<Self> {
+    pub fn new(top: &Topology, cpumasks: &[String]) -> Result<Self> {
         let mut span = Cpumask::new()?;
         let mut dom_numa_map = BTreeMap::new();
         // Track the domain ID separate from the LLC ID, because LLC IDs can

commit af1d23e699eb5da9ed4400f517679ee5a1cd5c3f

scx_bpfland: drop unused variable
With the global scx_utils::NR_CPU_IDS we don't need Topology anymore in
init_primary_domain(), so drop the variable to fix the following build
warning:

warning: unused variable: `topo`
   --> src/main.rs:385:9
    |
385 |         topo: &Topology,
    |         ^^^^ help: if this is intentional, prefix it with an underscore: `_topo`
    |
    = note: `#[warn(unused_variables)]` on by default

Fixes: 1da249f ("scx_utils::topology: Always use NR_CPU_IDS and NR_CPUS_POSSIBLE")
Signed-off-by: Andrea Righi <andrea.righi@linux.dev>

scheds/rust/scx_bpfland/src/main.rs
@@ -328,7 +328,7 @@ impl<'a> Scheduler<'a> {
         let topo = Topology::new().unwrap();
 
         // Initialize the primary scheduling domain (based on the --primary-domain option).
-        Self::init_primary_domain(&mut skel, &topo, &opts.primary_domain)?;
+        Self::init_primary_domain(&mut skel, &opts.primary_domain)?;
 
         // Initialize L2 cache domains.
         if !opts.disable_l2 {
@@ -383,7 +383,6 @@ impl<'a> Scheduler<'a> {
 
     fn init_primary_domain(
         skel: &mut BpfSkel<'_>,
-        topo: &Topology,
         primary_domain: &Cpumask,
     ) -> Result<()> {
         info!("primary CPU domain = 0x{:x}", primary_domain);