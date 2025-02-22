RitzDaCat/scx

commit 97495cedfa4afaec98c8a242a1672091a9c88eda

scx_bpfland: Replace bpf_ktime_get_ns() to scx_bpf_now()
Signed-off-by: Changwoo Min <changwoo@igalia.com>

scheds/rust/scx_bpfland/src/bpf/main.bpf.c
@@ -830,7 +830,7 @@ void BPF_STRUCT_OPS(bpfland_dispatch, s32 cpu, struct task_struct *prev)
  */
 static void update_cpuperf_target(struct task_struct *p, struct task_ctx *tctx)
 {
-	u64 now = bpf_ktime_get_ns();
+	u64 now = scx_bpf_now();
 	s32 cpu = scx_bpf_task_cpu(p);
 	u64 perf_lvl, delta_runtime, delta_t;
 	struct cpu_ctx *cctx;
@@ -882,7 +882,7 @@ static void update_cpuperf_target(struct task_struct *p, struct task_ctx *tctx)
 	 */
 	scx_bpf_cpuperf_set(cpu, perf_lvl);
 
-	cctx->last_running = bpf_ktime_get_ns();
+	cctx->last_running = scx_bpf_now();
 	cctx->prev_runtime = cctx->tot_runtime;
 }
 
@@ -895,7 +895,7 @@ void BPF_STRUCT_OPS(bpfland_running, struct task_struct *p)
 	tctx = try_lookup_task_ctx(p);
 	if (!tctx)
 		return;
-	tctx->last_run_at = bpf_ktime_get_ns();
+	tctx->last_run_at = scx_bpf_now();
 
 	/*
 	 * Adjust target CPU frequency before the task starts to run.
@@ -921,7 +921,7 @@ void BPF_STRUCT_OPS(bpfland_running, struct task_struct *p)
  */
 void BPF_STRUCT_OPS(bpfland_stopping, struct task_struct *p, bool runnable)
 {
-	u64 now = bpf_ktime_get_ns(), slice;
+	u64 now = scx_bpf_now(), slice;
 	s32 cpu = scx_bpf_task_cpu(p);
 	s64 delta_t;
 	struct cpu_ctx *cctx;
@@ -1043,7 +1043,7 @@ void BPF_STRUCT_OPS(bpfland_set_cpumask, struct task_struct *p,
 
 void BPF_STRUCT_OPS(bpfland_enable, struct task_struct *p)
 {
-	u64 now = bpf_ktime_get_ns();
+	u64 now = scx_bpf_now();
 	struct task_ctx *tctx;
 
 	/* Initialize task's vruntime */

commit 505b0bc76b0e50386f200a16f447a7e3bf76a5b5

scx_bpfland; use scx_bpf_now() consistently
Always use scx_bpf_now() to track time consistently. This fixes a bad
regression with the 6.14 kernel, as scx_bpf_now() is no longer
redirected to bpf_ktime_get_ns() and comparing the TSC timestamp with
the timestamp cached in struct rq can lead to inaccurate time
measurements.

Fixes: 97495ced ("scx_bpfland: Replace bpf_ktime_get_ns() to scx_bpf_now()")
Signed-off-by: Andrea Righi <arighi@nvidia.com>

scheds/rust/scx_bpfland/src/bpf/main.bpf.c
@@ -1022,7 +1022,7 @@ void BPF_STRUCT_OPS(bpfland_stopping, struct task_struct *p, bool runnable)
 	/*
 	 * Evaluate the time slice used by the task.
 	 */
-	slice = bpf_ktime_get_ns() - tctx->last_run_at;
+	slice = scx_bpf_now() - tctx->last_run_at;
 
 	/*
 	 * Update task's execution time (exec_runtime), but never account