RitzDaCat/scx

commit abbc4c2349a99e00de3fb673b15b18f89eea4895

scx_bpfland: Introduce --idle-resume-us
Introduce an option to configure the CPU idle QoS latency when the
scheduler is loaded.

Signed-off-by: Andrea Righi <arighi@nvidia.com>

scheds/rust/scx_bpfland/src/main.rs
@@ -32,6 +32,7 @@ use scx_stats::prelude::*;
 use scx_utils::autopower::{fetch_power_profile, PowerProfile};
 use scx_utils::build_id;
 use scx_utils::compat;
+use scx_utils::pm::{cpu_idle_resume_latency_supported, update_cpu_idle_resume_latency};
 use scx_utils::scx_ops_attach;
 use scx_utils::scx_ops_load;
 use scx_utils::scx_ops_open;
@@ -133,6 +134,14 @@ struct Opts {
     /// making the task ordering closer to a FIFO).
     #[clap(short = 'l', long, allow_hyphen_values = true, default_value = "20000")]
     slice_us_lag: i64,
+
+    /// Set CPU idle QoS resume latency in microseconds (-1 = disabled).
+    ///
+    /// Setting a lower latency value makes CPUs less likely to enter deeper idle states, enhancing
+    /// performance at the cost of higher power consumption. Alternatively, increasing the latency
+    /// value may reduce performance, but also improve power efficiency.
+    #[clap(short = 'I', long, allow_hyphen_values = true, default_value = "-1")]
+    idle_resume_us: i64,
 
     /// Disable preemption.
     ///
@@ -251,6 +260,20 @@ impl<'a> Scheduler<'a> {
                 "SMT off"
             }
         );
+
+        if opts.idle_resume_us >= 0 {
+            if !cpu_idle_resume_latency_supported() {
+                warn!("idle resume latency not supported");
+            } else {
+                info!("Setting idle QoS to {} us", opts.idle_resume_us);
+                for cpu in topo.all_cpus.values() {
+                    update_cpu_idle_resume_latency(
+                        cpu.id,
+                        opts.idle_resume_us.try_into().unwrap(),
+                    )?;
+                }
+            }
+        }
 
         // Initialize BPF connector.
         let mut skel_builder = BpfSkelBuilder::default();
@@ -605,6 +628,19 @@ impl<'a> Scheduler<'a> {
 impl Drop for Scheduler<'_> {
     fn drop(&mut self) {
         info!("Unregister {} scheduler", SCHEDULER_NAME);
+
+        // Restore default CPU idle QoS resume latency.
+        if self.opts.idle_resume_us >= 0 {
+            if cpu_idle_resume_latency_supported() {
+                let default_latency = 5000;
+                info!("Restore idle QoS to {} us", default_latency);
+
+                let topo = Topology::new().unwrap();
+                for cpu in topo.all_cpus.values() {
+                    update_cpu_idle_resume_latency(cpu.id, default_latency).unwrap();
+                }
+            }
+        }
     }
 }
 

commit 5039ff930f91806c7532169187697c2ce9abcd05

scx_bpfland: Restore the correct value of pm_qos_resume_latency_us
When --idle-resume-us is used, we are changing the global system
settings of the idle CPU resume latency and when the scheduler is
stopped we are implicitly resetting the latency to 5000 us, assuming
that is the default.

However, the default can be different across architectures or it can be
explicitly set by the user.

Therefore, make sure to restore the same previous value of
pm_qos_resume_latency_us when the scheduler is stopped.

Fixes: abbc4c23 ("scx_bpfland: Introduce --idle-resume-us")
Signed-off-by: Andrea Righi <arighi@nvidia.com>

scheds/rust/scx_bpfland/src/main.rs
@@ -234,6 +234,7 @@ struct Scheduler<'a> {
     skel: BpfSkel<'a>,
     struct_ops: Option<libbpf_rs::Link>,
     opts: &'a Opts,
+    topo: Topology,
     power_profile: PowerProfile,
     stats_server: StatsServer<(), Metrics>,
     user_restart: bool,
@@ -343,6 +344,7 @@ impl<'a> Scheduler<'a> {
             skel,
             struct_ops,
             opts,
+            topo,
             power_profile,
             stats_server,
             user_restart: false,
@@ -632,12 +634,9 @@ impl Drop for Scheduler<'_> {
         // Restore default CPU idle QoS resume latency.
         if self.opts.idle_resume_us >= 0 {
             if cpu_idle_resume_latency_supported() {
-                let default_latency = 5000;
-                info!("Restore idle QoS to {} us", default_latency);
-
-                let topo = Topology::new().unwrap();
-                for cpu in topo.all_cpus.values() {
-                    update_cpu_idle_resume_latency(cpu.id, default_latency).unwrap();
+                for cpu in self.topo.all_cpus.values() {
+                    update_cpu_idle_resume_latency(cpu.id, cpu.pm_qos_resume_latency_us as i32)
+                        .unwrap();
                 }
             }
         }