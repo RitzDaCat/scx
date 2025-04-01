RitzDaCat/scx

commit b7af9db3c0f38c57299fd71fb0bc285c42677e1d

scx_bpfland: Handle ttwu_queue case in ops.enqueue()
Always try to migrate and perform a direct dispatch from ops.enqueue()
in the case of a queued task wakeup (ttwu_queue): this can help reducing
the pressure on the per-node DSQs, which might have a significant impact
on systems with lots of CPUs within the same node.

Tested-by: Mattia Formichetti <mattiaformichetti@gmail.com>
Signed-off-by: Andrea Righi <arighi@nvidia.com>

scheds/rust/scx_bpfland/src/bpf/main.bpf.c
@@ -759,18 +759,39 @@ static bool try_direct_dispatch(struct task_struct *p, struct task_ctx *tctx,
 		}
 
 		/*
-		 * If the local DSQ and the shared DSQ have no task waiting
-		 * and the CPU is still a full-idle SMT core, perform a
-		 * direct dispatch.
+		 * Skip direct dispatch if the shared DSQ has tasks waiting
+		 * to prevent starvation.
 		 */
-		if (!scx_bpf_dsq_nr_queued(SCX_DSQ_LOCAL_ON | prev_cpu) &&
-		    (local_pcpu || !scx_bpf_dsq_nr_queued(node)) &&
-		    is_fully_idle(prev_cpu)) {
+		if (!local_pcpu && scx_bpf_dsq_nr_queued(node))
+			return false;
+
+		/*
+		 * If the previously used CPU is still a full-idle SMT
+		 * core, perform a direct dispatch.
+		 */
+		if (is_fully_idle(prev_cpu)) {
 			scx_bpf_dsq_insert(p, SCX_DSQ_LOCAL_ON | prev_cpu,
 					   slice_max, enq_flags);
 			__sync_fetch_and_add(&nr_direct_dispatches, 1);
 
 			return true;
+		}
+
+		/*
+		 * In case of a remote wakeup (ttwu_queue), attempt a task
+		 * migration.
+		 */
+		if (!scx_bpf_task_running(p)) {
+			bool is_idle = false;
+			s32 cpu;
+
+			cpu = pick_idle_cpu(p, prev_cpu, 0, &is_idle);
+			if (is_idle) {
+				scx_bpf_dsq_insert(p, SCX_DSQ_LOCAL, slice_max, 0);
+				__sync_fetch_and_add(&nr_direct_dispatches, 1);
+
+				return true;
+			}
 		}
 	}
 

commit 261598daa7e35e357e280a10775194ad01c6e5ea

scx_bpfland: Fix direct dispatch migration
During direct dispatch from ops.enqueue(), move the task to the right
picked CPU, if a new idle CPU is found for migration.

Moreover, make sure to ignore migration attempts with per-CPU tasks and
always keep them running on the same CPU if it's idle.

Fixes: b7af9db3 ("scx_bpfland: Handle ttwu_queue case in ops.enqueue()")
Signed-off-by: Andrea Righi <arighi@nvidia.com>

scheds/rust/scx_bpfland/src/bpf/main.bpf.c
@@ -771,7 +771,7 @@ static bool try_direct_dispatch(struct task_struct *p, struct task_ctx *tctx,
 		 */
 		if (p->nr_cpus_allowed == 1) {
 			if (scx_bpf_test_and_clear_cpu_idle(prev_cpu)) {
-				scx_bpf_dsq_insert(p, SCX_DSQ_LOCAL_ON | prev_cpu,
+				scx_bpf_dsq_insert(p, SCX_DSQ_LOCAL,
 						   slice_max, enq_flags);
 				__sync_fetch_and_add(&nr_direct_dispatches, 1);
 
@@ -808,7 +808,7 @@ static bool try_direct_dispatch(struct task_struct *p, struct task_ctx *tctx,
 
 			cpu = pick_idle_cpu(p, prev_cpu, 0, &is_idle);
 			if (is_idle) {
-				scx_bpf_dsq_insert(p, SCX_DSQ_LOCAL, slice_max, 0);
+				scx_bpf_dsq_insert(p, SCX_DSQ_LOCAL_ON | cpu, slice_max, 0);
 				__sync_fetch_and_add(&nr_direct_dispatches, 1);
 
 				return true;
@@ -891,6 +891,12 @@ static bool keep_running(const struct task_struct *p, s32 cpu)
 	 * is disabled).
 	 */
 	if (!smt_enabled)
+		return true;
+
+	/*
+	 * If the task can only run on this CPU, keep it running.
+	 */
+	if (p->nr_cpus_allowed == 1)
 		return true;
 
 	cctx = try_lookup_cpu_ctx(cpu);