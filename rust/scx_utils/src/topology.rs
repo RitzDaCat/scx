RitzDaCat/scx

commit 98f24eaa45166b7ac01f0c7ef01c471f0e2195b9

scx_utils: Create Cpus in a numerical order.
Previously, the CPUs are created in CPU id's lexicographical order (e.g., 1 ->
10 -> 11 -> 2), so core ids are assigned following the lexicographical ordering.
There is nothing wrong with it, but it is unintuitive. Thus, let's iterate and
create CPUs in numerical order, so assign core id in an intuitive order.

Signed-off-by: Changwoo Min <changwoo@igalia.com>

rust/scx_utils/src/topology.rs
@@ -785,22 +785,13 @@ fn create_numa_nodes(
             }
         }
 
-        let cpu_pattern = numa_path.join("cpu[0-9]*");
-        let cpu_paths = glob(cpu_pattern.to_string_lossy().as_ref())?;
         let big_little = has_big_little().unwrap_or(false);
         let capacity_src = cpu_capacity_source();
         let avg_cpu_freq = avg_cpu_freq();
-        for cpu_path in cpu_paths.filter_map(Result::ok) {
-            let cpu_str = cpu_path.to_str().unwrap().trim();
-            let cpu_id = match sscanf!(cpu_str, "/sys/devices/system/node/node{usize}/cpu{usize}") {
-                Ok((_, val)) => val,
-                Err(_) => {
-                    bail!("Failed to parse cpu ID {}", cpu_str);
-                }
-            };
-
+        let cpu_ids = read_cpu_ids()?;
+        for cpu_id in cpu_ids.iter() {
             create_insert_cpu(
-                cpu_id,
+                *cpu_id,
                 &mut node,
                 online_mask,
                 topo_ctx,

commit 34ad0d096978f17461fa86d3f7b400dfb28581cf

scx_utils: Fix incorrect handling of CPU IDs on multi-NUMA machines.
The previous handling of CPU IDs for natural ordering ignored the NUMA
boundaries, causing an error on multi-NUMA machines. So, pursue the numerical
ordering of CPU IDs within a NUMA domain.

Fixes: 98f24eaa ("scx_utils: Create Cpus in a numerical order.")
Reported-by: Andrea Righi <arighi@nvidia.com>

rust/scx_utils/src/topology.rs
@@ -785,13 +785,27 @@ fn create_numa_nodes(
             }
         }
 
+        let cpu_pattern = numa_path.join("cpu[0-9]*");
+        let cpu_paths = glob(cpu_pattern.to_string_lossy().as_ref())?;
         let big_little = has_big_little().unwrap_or(false);
         let capacity_src = cpu_capacity_source();
         let avg_cpu_freq = avg_cpu_freq();
-        let cpu_ids = read_cpu_ids()?;
-        for cpu_id in cpu_ids.iter() {
+        let mut cpu_ids = vec![];
+        for cpu_path in cpu_paths.filter_map(Result::ok) {
+            let cpu_str = cpu_path.to_str().unwrap().trim();
+            let cpu_id = match sscanf!(cpu_str, "/sys/devices/system/node/node{usize}/cpu{usize}") {
+                Ok((_, val)) => val,
+                Err(_) => {
+                    bail!("Failed to parse cpu ID {}", cpu_str);
+                }
+            };
+            cpu_ids.push(cpu_id);
+        }
+        cpu_ids.sort();
+
+        for cpu_id in cpu_ids {
             create_insert_cpu(
-                *cpu_id,
+                cpu_id,
                 &mut node,
                 online_mask,
                 topo_ctx,