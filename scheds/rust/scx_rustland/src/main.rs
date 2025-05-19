RitzDaCat/scx

commit f3f24feb7bf04d7adee05f0a8c3b4a77e83eeb7e

scx_rustland: Introduce a congestion threshold
If too many tasks are piling up in the user-space scheduler we may risk
to hit stall conditions.

To prevent this, introduce a congestion threshold: when the number of
waiting tasks exceeds this threshold, the scheduler will proactively
flush the queue to bring the task count back below the critical level.

Moreover, introduce the new option --nr-waiting-max to make this
threshold configurable from the command line.

This helps handle heavy stress tests that might flood the system with a
high volume of tasks.

Signed-off-by: Andrea Righi <arighi@nvidia.com>

scheds/rust/scx_rustland/src/main.rs
@@ -82,6 +82,16 @@ struct Opts {
     #[clap(short = 'S', long, default_value = "1000")]
     slice_us_min: u64,
 
+    /// Specifies the maximum number of tasks that can be queued in the user-space scheduler.
+    /// If this limit is exceeded, the scheduler will attempt to flush tasks until the count falls
+    /// below the threshold.
+    ///
+    /// Lowering this value reduces the effectiveness of the scheduler under heavy load, while
+    /// raising it can improve scheduling efficiency. However, a higher limit may also reduce
+    /// robustness and increase the risk of stalls when the system is overloaded.
+    #[clap(short = 'w', long, default_value = "100")]
+    nr_waiting_max: u64,
+
     /// If specified, only tasks which have their scheduling policy set to SCHED_EXT using
     /// sched_setscheduler(2) are switched. Otherwise, all tasks are switched.
     #[clap(short = 'p', long, action = clap::ArgAction::SetTrue)]
@@ -210,8 +220,9 @@ struct Scheduler<'a> {
     task_map: TaskInfoMap,                  // map pids to the corresponding task information
     min_vruntime: u64,                      // Keep track of the minimum vruntime across all tasks
     init_page_faults: u64,                  // Initial page faults counter
-    slice_ns: u64,                          // Default time slice (in ns)
-    slice_ns_min: u64,                      // Minimum time slice (in ns)
+    nr_waiting_max: u64, // Maximum amount of tasks allowed to wait in the scheduler
+    slice_ns: u64,       // Default time slice (in ns)
+    slice_ns_min: u64,   // Minimum time slice (in ns)
 }
 
 impl<'a> Scheduler<'a> {
@@ -237,6 +248,7 @@ impl<'a> Scheduler<'a> {
             task_map: TaskInfoMap::new(),
             min_vruntime: 0,
             init_page_faults: 0,
+            nr_waiting_max: opts.nr_waiting_max,
             slice_ns: opts.slice_us * NSEC_PER_USEC,
             slice_ns_min: opts.slice_us_min * NSEC_PER_USEC,
         })
@@ -273,6 +285,21 @@ impl<'a> Scheduler<'a> {
             .duration_since(SystemTime::UNIX_EPOCH)
             .unwrap();
         ts.as_nanos() as u64
+    }
+
+    // Return the total amount of tasks waiting in the user-space scheduler.
+    fn nr_tasks_scheduled(&mut self) -> u64 {
+        self.task_pool.tasks.len() as u64
+    }
+
+    // Return the total amount of tasks waiting to be consumed by the user-space scheduler.
+    fn nr_tasks_queued(&mut self) -> u64 {
+        *self.bpf.nr_queued_mut()
+    }
+
+    // Return the total amount of tasks that are waiting to be scheduled.
+    fn nr_tasks_waiting(&mut self) -> u64 {
+        self.nr_tasks_queued() + self.nr_tasks_scheduled()
     }
 
     // Update task's vruntime based on the information collected from the kernel and return to the
@@ -317,49 +344,16 @@ impl<'a> Scheduler<'a> {
         task_info.vruntime + task.exec_runtime.min(self.slice_ns * 100)
     }
 
-    // Drain all the tasks from the queued list, update their vruntime (Self::update_enqueued()),
-    // then push them all to the task pool (doing so will sort them by their vruntime).
-    fn drain_queued_tasks(&mut self) {
-        loop {
-            match self.bpf.dequeue_task() {
-                Ok(Some(task)) => {
-                    // Update task information and determine vruntime.
-                    let deadline = self.update_enqueued(&task);
-                    let timestamp = Self::now();
-
-                    // Insert task in the task pool (ordered by vruntime).
-                    self.task_pool.push(Task {
-                        qtask: task,
-                        deadline,
-                        timestamp,
-                    });
-                }
-                Ok(None) => {
-                    break;
-                }
-                Err(err) => {
-                    warn!("Error: {}", err);
-                    break;
-                }
-            }
-        }
-    }
-
-    // Return the total amount of tasks that are waiting to be scheduled.
-    fn nr_tasks_waiting(&mut self) -> u64 {
-        let nr_queued = *self.bpf.nr_queued_mut();
-        let nr_scheduled = *self.bpf.nr_scheduled_mut();
-
-        nr_queued + nr_scheduled
-    }
-
     // Dispatch the first task from the task pool (sending them to the BPF dispatcher).
-    fn dispatch_tasks(&mut self) {
+    //
+    // Return true on success, false if the BPF backend can't accept any more dispatch.
+    fn dispatch_task(&mut self) -> bool {
+        let nr_waiting = self.nr_tasks_waiting() + 1;
+
         if let Some(task) = self.task_pool.pop() {
             // Scale time slice based on the amount of tasks that are waiting in the
             // scheduler's queue and the previously unused time slice budget, but make sure
             // to assign at least slice_us_min.
-            let nr_waiting = self.nr_tasks_waiting() + 1;
             let slice_ns = (self.slice_ns / nr_waiting).max(self.slice_ns_min);
 
             // Create a new task to dispatch.
@@ -381,15 +375,58 @@ impl<'a> Scheduler<'a> {
             if self.bpf.dispatch_task(&dispatched_task).is_err() {
                 // If dispatching fails, re-add the task to the pool and skip further dispatching.
                 self.task_pool.push(task);
+
+                return false;
             }
         }
+
+        return true;
+    }
+
+    // Drain all the tasks from the queued list, update their vruntime (Self::update_enqueued()),
+    // then push them all to the task pool (doing so will sort them by their vruntime).
+    fn drain_queued_tasks(&mut self) {
+        loop {
+            match self.bpf.dequeue_task() {
+                Ok(Some(task)) => {
+                    // Update task information and determine vruntime.
+                    let deadline = self.update_enqueued(&task);
+                    let timestamp = Self::now();
+
+                    // Insert task in the task pool (ordered by vruntime).
+                    self.task_pool.push(Task {
+                        qtask: task,
+                        deadline,
+                        timestamp,
+                    });
+
+                    // Do not allow too many tasks to pile up in the user-space scheduler, if that
+                    // happens flush the first task immediately, until we get back below the
+                    // critical threshold.
+                    if self.nr_tasks_scheduled() >= self.nr_waiting_max {
+                        if !self.dispatch_task() {
+                            break;
+                        }
+                    }
+                }
+                Ok(None) => {
+                    break;
+                }
+                Err(err) => {
+                    warn!("Error: {}", err);
+                    break;
+                }
+            }
+        }
+
+        // Dispatch the first task from the task pool.
+        self.dispatch_task();
     }
 
     // Main scheduling function (called in a loop to periodically drain tasks from the queued list
     // and dispatch them to the BPF part via the dispatched list).
     fn schedule(&mut self) {
         self.drain_queued_tasks();
-        self.dispatch_tasks();
 
         // Notify the dispatcher if there are still pending tasks to be processed,
         self.bpf.notify_complete(self.task_pool.tasks.len() as u64);

commit 0b7fb163a6add2ff5f79268b3737a04d1dc38a96

scx_rustland: Revert congestion threshold
Commit f3f24feb ("scx_rustland: Introduce a congestion threshold")
introduced a congestion threshold where the scheduler starts to dispatch
tasks more synchronously to avoid too many tasks piling up in the
scheduler's queue. Even if this can make the scheduler more robust under
certain conditions, it also makes it way less effective in other
overloaded scenarios.

Therefore drop the special congestion logic and always rely on the
scheduling policy.

Fixes: f3f24feb ("scx_rustland: Introduce a congestion threshold")
Tested-by: Mario Roy <marioeroy@gmail.com>
Signed-off-by: Andrea Righi <arighi@nvidia.com>

scheds/rust/scx_rustland/src/main.rs
@@ -12,7 +12,6 @@ use bpf::*;
 
 mod stats;
 use std::collections::BTreeSet;
-use std::collections::HashMap;
 use std::fs::File;
 use std::io::Read;
 use std::io::{self};
@@ -81,16 +80,6 @@ struct Opts {
     #[clap(short = 'S', long, default_value = "1000")]
     slice_us_min: u64,
 
-    /// Specifies the maximum number of tasks that can be queued in the user-space scheduler.
-    /// If this limit is exceeded, the scheduler will attempt to flush tasks until the count falls
-    /// below the threshold.
-    ///
-    /// Lowering this value reduces the effectiveness of the scheduler under heavy load, while
-    /// raising it can improve scheduling efficiency. However, a higher limit may also reduce
-    /// robustness and increase the risk of stalls when the system is overloaded.
-    #[clap(short = 'w', long, default_value = "100")]
-    nr_waiting_max: u64,
-
     /// If specified, only tasks which have their scheduling policy set to SCHED_EXT using
     /// sched_setscheduler(2) are switched. Otherwise, all tasks are switched.
     #[clap(short = 'p', long, action = clap::ArgAction::SetTrue)]
@@ -152,9 +141,8 @@ struct Scheduler<'a> {
     tasks: BTreeSet<Task>,                  // tasks ordered by deadline
     min_vruntime: u64,                      // Keep track of the minimum vruntime across all tasks
     init_page_faults: u64,                  // Initial page faults counter
-    nr_waiting_max: u64, // Maximum amount of tasks allowed to wait in the scheduler
-    slice_ns: u64,       // Default time slice (in ns)
-    slice_ns_min: u64,   // Minimum time slice (in ns)
+    slice_ns: u64,                          // Default time slice (in ns)
+    slice_ns_min: u64,                      // Minimum time slice (in ns)
 }
 
 impl<'a> Scheduler<'a> {
@@ -179,7 +167,6 @@ impl<'a> Scheduler<'a> {
             tasks: BTreeSet::new(),
             min_vruntime: 0,
             init_page_faults: 0,
-            nr_waiting_max: opts.nr_waiting_max,
             slice_ns: opts.slice_us * NSEC_PER_USEC,
             slice_ns_min: opts.slice_us_min * NSEC_PER_USEC,
         })
@@ -310,15 +297,6 @@ impl<'a> Scheduler<'a> {
                         deadline,
                         timestamp,
                     });
-
-                    // Do not allow too many tasks to pile up in the user-space scheduler, if that
-                    // happens flush the first task immediately, until we get back below the
-                    // critical threshold.
-                    if self.nr_tasks_scheduled() >= self.nr_waiting_max {
-                        if !self.dispatch_task() {
-                            break;
-                        }
-                    }
                 }
                 Ok(None) => {
                     break;