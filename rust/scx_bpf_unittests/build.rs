RitzDaCat/scx

commit 7d9b2cc26473526883297df78e8eee3f2e7b6194

scx_bpf_unittests: add crate to drive BPF unittests
Currently scxtest tests are driven by a main function handwritten in C
and ran by `meson test`. This is fine, but let's change it for a few
reasons:
- I want fewer dependencies on meson, not more.
- We already have a richer test runner, `cargo test`, that is used in
  the repo.
- There's no autodetection of tests available and every new test must be
  added to main.

This change builds the BPF unittests in a new crate,
`scx_bpf_unittests`, that doesn't get published. It uses codegen based
on this build and a new macro, `SCX_TEST`, to find the tests and
automatically shim them into Rust. `cargo test` then runs this userspace
code perfectly normally. As we already have `cargo test` in the CI, this
runs the tests in the CI, with full multithreading.

Changes made to the existing test framework:
- Add `SCX_TEST` macro with a `.scxtest` section to automatically locate
  all the built tests.
- Use a longjmp for `scx_test_assert` to avoid `abort()` as the Rust
  test runner doesn't like this and we still want to run other tests
  after one fails.
- Switch every test global I could find to `__thread`. We may need to be
  smarter about this in the future when we test scheduler methods that
  touch scheduler owned globals, but it should be fine for now.
- Runs `init_p2dq_test` in every test instead of once in `main` to make sure
  every thread is initialised properly.
- Rip out the meson logic. We should only have one way of running these
  tests and it should be the version we run in the CI to avoid
  diverging/confusion.

In future we should make this test builder a library and expose it to the crates
so these tests run as part of `scx_p2dq` instead of in a separate crate (keeping
a separate crate for the cscheds). For now, let's run them in their own crate to
let them bed in for a while.

Test plan:
- CI

rust/scx_bpf_unittests/build.rs
@@ -1,0 +1,120 @@
+// Copyright (c) Meta Platforms, Inc. and affiliates.
+
+// This software may be used and distributed according to the terms of the
+// GNU General Public License version 2.
+
+use indoc::formatdoc;
+use object::read::archive::ArchiveFile;
+use object::Object;
+use object::ObjectSection;
+use object::ObjectSymbol;
+
+use std::env;
+use std::fs;
+use std::io::Write;
+use std::path::PathBuf;
+
+fn main() {
+    let out_dir: PathBuf = env::var("OUT_DIR").unwrap().into();
+    let manifest_dir: PathBuf = env::var("CARGO_MANIFEST_DIR").unwrap().into();
+    let root_dir = manifest_dir.join("../..");
+
+    let include_path = &[
+        root_dir.join("lib/scxtest/"),
+        root_dir.join("scheds/include/"),
+        root_dir.join("scheds/include/arch/x86/"),
+        root_dir.join("scheds/include/bpf-compat/"),
+        env::var("DEP_LIBBPF_INCLUDE").unwrap().into(),
+    ];
+
+    // Build the C tests
+    cc::Build::new()
+        .compiler(env::var("BPF_CLANG").unwrap_or_else(|_| "clang".into()))
+        .files(&[root_dir.join("scheds/rust/scx_p2dq/src/bpf/main.test.bpf.c")])
+        .warnings(false)
+        .define("TEST", None)
+        .flags(&[
+            "-Wno-attributes",
+            "-Wno-unknown-pragmas",
+            "-Wno-incompatible-pointer-types",
+            "-Wno-unused-variable",
+        ])
+        .includes(include_path)
+        .compile("p2dq_tests");
+
+    // Build the support library - this has to come after p2dq_tests, and is a good reason to
+    // separate the crates.
+    cc::Build::new()
+        .compiler(env::var("BPF_CLANG").unwrap_or_else(|_| "clang".into()))
+        .files(&[
+            root_dir.join("lib/scxtest/scx_test.c"),
+            root_dir.join("lib/scxtest/overrides.c"),
+            root_dir.join("lib/scxtest/scx_test_map.c"),
+            root_dir.join("lib/scxtest/scx_test_cpumask.c"),
+        ])
+        .define("TEST", None)
+        .includes(include_path)
+        .compile("scxtest");
+
+    // Extract test names
+    let mut tests = vec![];
+
+    let archive_data = fs::read(&out_dir.join("libp2dq_tests.a")).unwrap();
+    let archive = ArchiveFile::parse(&*archive_data).unwrap();
+    for member in archive.members() {
+        let member = member.unwrap();
+
+        let obj_data = member.data(&*archive_data).unwrap();
+        let obj = object::File::parse(obj_data).unwrap();
+
+        let test_section_index = if let Some(s) = obj.section_by_name(".scxtest") {
+            s.index()
+        } else {
+            continue;
+        };
+
+        for symbol in obj.symbols() {
+            if let Some(i) = symbol.section_index() {
+                if i != test_section_index {
+                    continue;
+                }
+
+                let name = if let Ok(n) = symbol.name() {
+                    n.to_string()
+                } else {
+                    continue;
+                };
+
+                // unclear where the empty name comes from, filter it out
+                if name != "" {
+                    tests.push(name);
+                }
+            }
+        }
+    }
+
+    // Generate Rust wrappers for the tests
+    let mut test_content = fs::File::create(out_dir.join("gen_tests.rs")).unwrap();
+    for name in &tests {
+        test_content
+            .write_all(
+                formatdoc! {r#"
+                    extern "C" {{ fn {name}() -> i32; }}
+                    #[test]
+                    fn p2dq_{name}() -> ::std::result::Result<(), i32> {{
+                        let ret = unsafe {{ {name}() }};
+
+                        (ret == 0)
+                            .then(|| ())
+                            .ok_or(ret)
+                    }}
+        "#, name = name}
+                .as_bytes(),
+            )
+            .unwrap();
+    }
+
+    // Rebuild directives
+    println!("cargo:rerun-if-changed=../../lib/scxtest");
+    println!("cargo:rerun-if-changed=../../scheds/rust/scx_p2dq/src/bpf");
+}

Cargo.toml
@@ -1,5 +1,6 @@
 [workspace]
 members = [
+    "rust/scx_bpf_unittests",
     "rust/scx_lib_selftests",
     "rust/scx_loader",
     "rust/scx_rustland_core",

lib/meson.build
@@ -1,5 +1,3 @@
-subdir('scxtest')
-
 libs = ['sdt_alloc', 'sdt_task']
 
 objs = []
@@ -23,17 +21,3 @@ scx_lib = custom_target(scx_lib_name,
   depends: objs,
 )
 
-test_sources = ['arena.bpf.c', 'bitmap.bpf.c', 'cpumask.bpf.c', 'sdt_alloc.bpf.c', 'sdt_task.bpf.c', 'topology.bpf.c']
-
-scx_lib_test_obj = static_library('scxlib_test',
-  test_sources,
-  include_directories: include_directories(['.', 'scxtest']),
-  c_args: scx_test_c_args,
-  dependencies: [kernel_dep, libbpf_dep, user_c_dep, scxtest_dep],
-)
-
-scx_lib_test_dep = declare_dependency(
-  link_with: scx_lib_test_obj,
-  include_directories: include_directories(['.']),
-
-)

lib/scxtest/meson.build
@@ -1,13 +1,0 @@
-scxtest_lib = static_library(
-  'scxtest',
-  'scx_test.c',
-  'overrides.c',
-  'scx_test_map.c',
-  'scx_test_cpumask.c',
-  c_args: ['-DTEST'],
-)
-
-scxtest_dep = declare_dependency(
-  link_with: scxtest_lib,
-  include_directories: include_directories(['.']),
-)

lib/scxtest/overrides.c
@@ -10,7 +10,9 @@ struct cpumask;
 struct task_struct;
 
 __weak
-void scx_bpf_error_bstr(char *fmt, long long unsigned int *data, u32 data__sz)
+void scx_bpf_error_bstr(char *fmt __attribute__((unused)),
+		        long long unsigned int *data __attribute__((unused)),
+			u32 data__sz __attribute__((unused)))
 {
 }
 
@@ -21,40 +23,42 @@ struct bpf_cpumask *bpf_cpumask_create(void)
 }
 
 __weak
-void bpf_cpumask_release(struct bpf_cpumask *cpumask)
+void bpf_cpumask_release(struct bpf_cpumask *cpumask __attribute__((unused)))
 {
 }
 
 __weak
-void bpf_task_release(struct task_struct *p)
+void bpf_task_release(struct task_struct *p __attribute__((unused)))
 {
 }
 
 __weak
-s32 scx_bpf_task_cpu(const struct task_struct *p)
+s32 scx_bpf_task_cpu(const struct task_struct *p __attribute__((unused)))
 {
 	return -1;
 }
 
 __weak
-struct task_struct *bpf_task_from_pid(s32 pid)
+struct task_struct *bpf_task_from_pid(s32 pid __attribute__((unused)))
 {
 	return NULL;
 }
 
 __weak
-s32 scx_bpf_dsq_nr_queued(u64 dsq_id)
+s32 scx_bpf_dsq_nr_queued(u64 dsq_id __attribute__((unused)))
 {
 	return -1;
 }
 
 __weak
-void scx_bpf_kick_cpu(s32 cpu, u64 flags)
+void scx_bpf_kick_cpu(s32 cpu __attribute__((unused)),
+		      u64 flags __attribute__((unused)))
 {
 }
 
 __weak
-s32 scx_bpf_create_dsq(u64 dsq_id, s32 node)
+s32 scx_bpf_create_dsq(u64 dsq_id __attribute__((unused)),
+		       s32 node __attribute__((unused)))
 {
 	return -1;
 }
@@ -70,29 +74,39 @@ void bpf_rcu_read_unlock(void)
 }
 
 __weak
-void bpf_cpumask_set_cpu(u32 cpu, struct bpf_cpumask *cpumask)
+void bpf_cpumask_set_cpu(u32 cpu __attribute__((unused)),
+		         struct bpf_cpumask *cpumask __attribute__((unused)))
 {
 }
 
 __weak
-u32 bpf_cpumask_any_distribute(const struct cpumask *cpumask)
+u32 bpf_cpumask_any_distribute(const struct cpumask *cpumask __attribute__((unused)))
 {
 	return 0;
 }
 
 __weak
-bool bpf_cpumask_and(struct bpf_cpumask *dst, const struct cpumask *src1, const struct cpumask *src2)
+bool bpf_cpumask_and(struct bpf_cpumask *dst __attribute__((unused)),
+		     const struct cpumask *src1 __attribute__((unused)),
+		     const struct cpumask *src2 __attribute__((unused)))
 {
 	return false;
 }
 
 __weak
-u32 bpf_cpumask_weight(const struct cpumask *cpumask)
+u32 bpf_cpumask_weight(const struct cpumask *cpumask __attribute__((unused)))
 {
 	return 0;
 }
 
 __weak
-void scx_bpf_put_cpumask(const struct cpumask *cpumask)
+void scx_bpf_put_cpumask(const struct cpumask *cpumask __attribute__((unused)))
 {
+}
+
+__weak
+void *scx_task_data(struct task_struct *p __attribute__((unused)))
+{
+	// No arena support in scxtest yet, we can drop this when it's available.
+	return NULL;
 }

lib/scxtest/overrides.h
@@ -13,7 +13,7 @@
  * that we want to get rid of that belongs here.
  */
 #define __builtin_preserve_field_info(x,y) 1
-#define __builtin_preserve_enum_value(x,y,z) 1
+#define __builtin_preserve_enum_value(x,y) 1
 
 #define bpf_addr_space_cast(var, dst_as, src_as)
 

lib/scxtest/scx_test.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 #include "scx_test.h"
+
+__thread jmp_buf scxtest_bail_jmp;
 
 void __fail_assert(const char *condition, const char *file, int line)
 {
 	fprintf(stderr, "Assertion failed: %s, file %s, line %d\n", condition, file, line);
-	abort();
+	longjmp(scxtest_bail_jmp, -1);
 }

lib/scxtest/scx_test.h
@@ -8,6 +8,10 @@
 
 #include "overrides.h"
 
+#include <setjmp.h>
+
+extern __thread jmp_buf scxtest_bail_jmp;
+
 void __fail_assert(const char *condition, const char *file, int line) __attribute__((noreturn));
 
 #define scx_test_assert(condition) \
@@ -15,5 +19,17 @@ void __fail_assert(const char *condition, const char *file, int line) __attribut
 		if (!(condition)) \
 			__fail_assert(#condition, __FILE__, __LINE__); \
 	} while (0)
+
+#define SCX_TEST(name) \
+	static __always_inline void name##_scxtest_impl(void);	\
+	__attribute__((used))					\
+	__attribute__((section(".scxtest")))			\
+	int name(void) {					\
+		int rc = setjmp(scxtest_bail_jmp);		\
+		if (!rc)					\
+			name##_scxtest_impl();			\
+		return rc;					\
+	}							\
+	static __always_inline void name##_scxtest_impl(void)
 
 #endif /* TEST */

commit 6770f2120c6f6879284440726e0c2defc5718363

scx_bpf_unittests: Filter invalid symbol names in test codegen
Commit 7d9b2cc2 introduced a build issue where invalid symbols like "$x"
from the `.scxtest` ELF section could be inserted into the generated
`gen_tests.rs`, leading to compilation failures.

This patch filters out any non-identifier-compatible symbol names before
generating test wrappers.

Test plan:
- Run `cargo test -p scx_bpf_unittests`.

Fixes: 7d9b2cc26473 ("scx_bpf_unittests: add crate to drive BPF unittests")
Suggested-by: Jake Hillion <jake@hillion.co.uk>
Signed-off-by: Cheng-Yang Chou <yphbchou0911@gmail.com>

rust/scx_bpf_unittests/build.rs
@@ -86,7 +86,7 @@ fn main() {
                 };
 
                 // unclear where the empty name comes from, filter it out
-                if name != "" {
+                if symbol.is_definition() {
                     tests.push(name);
                 }
             }